e74b6814793151dd6c37d3b78d0c29aa7dbe527c
Kseniia111 <kseniya.markovych@gmail.com>
2023-10-11 20:27:50 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -29,33 +29,3 @@ function Modal({ largeImageURL, onToggleModal }) {
   );
 }
 export default Modal;
-
-// const Modal = {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleBackdropClick = e => {
-//     if (e.currentTarget === e.target) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <div className={css.overlay} onClick={this.handleBackdropClick}>
-//         <div className={css.modal}>{this.props.children}</div>
-//       </div>
-//     );
-//   }
-// }
